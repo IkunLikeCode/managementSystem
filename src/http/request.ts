cf67c9822bdd183da9dfd979605a20a6e689f1fb
yaobo <15170970773@163.com>
2025-06-04 20:54:08 +0800

    对axios进行了封装

--- src/http/request.ts
+++ src/http/request.ts
@@ -1,10 +1,14 @@
 import axios from "axios";
+import { getMessageInfo } from "@/utils/statusCode";
 import type {
   AxiosInstance,
   AxiosResponse,
   AxiosError,
   InternalAxiosRequestConfig,
+  AxiosRequestConfig,
 } from "axios";
+import { ElMessage } from "element-plus";
+import { BaseResponse } from "@/utils/types";
 
 const service: AxiosInstance = axios.create({
   baseURL: Boolean(import.meta.env.VITE_APP_USE_MOCK)
@@ -17,7 +21,6 @@ const service: AxiosInstance = axios.create({
 service.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
     // 在发送请求之前做些什么
-    // 可以在这里添加请求头等
     return config;
   },
   (error: AxiosError) => {
@@ -28,13 +31,75 @@ service.interceptors.request.use(
 // 响应拦截器
 service.interceptors.response.use(
   (response: AxiosResponse) => {
-    // 对响应数据做点什么
-    // 可以在这里处理响应数据
+    if (response.status === 200) {
+      return response;
+    }
+    ElMessage({
+      message: getMessageInfo(response.status),
+      type: "error",
+      duration: 3000,
+    });
     return response.data;
   },
   (error: AxiosError) => {
-    // 对响应错误做点什么
-    // 可以在这里处理错误信息
-    return Promise.reject(error);
+    const { response } = error;
+    if (response) {
+      ElMessage({
+        message: getMessageInfo(response.status),
+        type: "error",
+        duration: 3000,
+      });
+      return Promise.reject(error);
+    }
+    ElMessage({
+      message: "网络异常，请稍后再试！",
+      type: "error",
+      duration: 3000,
+    });
   }
 );
+
+// 封装request方法
+export const requestInstace = <T = any>(
+  config: AxiosRequestConfig
+): Promise<T> => {
+  return new Promise((resolve, reject) => {
+    service
+      .request<any, AxiosResponse<BaseResponse>>({ ...config })
+      .then((res: AxiosResponse<BaseResponse>) => {
+        const data = res.data;
+        if (data.code !== 1) {
+          ElMessage({
+            message: data.message || "请求失败",
+            type: "error",
+            duration: 3000,
+          });
+          reject(new Error(data.message || "请求失败"));
+        } else {
+          ElMessage({
+            message: data.message || "请求成功",
+            type: "success",
+            duration: 3000,
+          });
+          resolve(data.data as T);
+        }
+      });
+  });
+};
+
+// 在最后使用封装过的axios导出不同的请求方式
+export function get<T = any, U = any>(
+  config: AxiosRequestConfig,
+  url: string,
+  parms?: U
+): Promise<T> {
+  return requestInstace({ ...config, url, method: "GET", params: parms });
+}
+
+export function post<T = any, U = any>(
+  config: AxiosRequestConfig,
+  url: string,
+  data: U
+): Promise<T> {
+  return requestInstace({ ...config, url, method: "POST", data: data });
+}
